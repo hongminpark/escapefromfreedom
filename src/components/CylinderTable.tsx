18ef1a7fe3a029a00cb6528b7db37ed195210b7c
hongminpark <zhdals1026@gmail.com>
2023-11-01 15:39:36 +0900

    Change AR button location

--- src/components/CylinderTable.tsx
+++ src/components/CylinderTable.tsx
@@ -59,10 +59,18 @@ const CylinderTable = () => {
               display: "block",
               margin: "0 auto",
             }}
-          />
+          >
+            <button
+              slot="ar-button"
+              style={{
+                position: "absolute",
+                top: "16px",
+                right: "16px",
+              }}
+            ></button>
+          </model-viewer>
         </div>
-
-        <div className="flex justify-center space-x-2">
+        <div className="flex justify-center space-x-2 py-4">
           {colors.map((color) => (
             <button
               key={color.code}
